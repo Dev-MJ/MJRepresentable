de7981b49ae4befa20d62100d7bf878a67be8d3d
Dev.MJ <mr.lucifers@gmail.com>
2024-01-05 15:45:24 +0900

    sampleapp 추가

---
+++ MJRepresentable/MJRepresentableExampleApp/MJRepresentableExampleApp/MJRepresentableExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MJRepresentableExampleAppApp.swift
+//  MJRepresentableExampleApp
+//
+//  Created by mj.lee on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MJRepresentableExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
